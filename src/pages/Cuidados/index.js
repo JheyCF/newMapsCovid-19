3228080945e04dc7c0a72aacaf6af4c23b7ec9c5
JenniferSilva46 <jennifer.silva@academico.ifpb.edu.br>
2020-11-22 12:04:37 -0300

    Configurando estrutura do repositorio

--- src/pages/Cuidados/index.js
+++ src/pages/Cuidados/index.js
@@ -1,47 +1,57 @@
-import React from 'react';
+import React from "react";
 
-import Logo from '../../components/logo-responsivo/index';
-import Menu from "../../components/Menu/index"
-import Rodape from '../../components/Rodape';
+import Previnir from "../../components/Menu/cuidados";
 
-import "./style.css"
+import "./style.css";
 
 function Cuidados() {
   return (
     <div className="page-cuidados">
       <div className="todo-menu">
         <nav>
-          <Menu />
+          <Previnir />
         </nav>
       </div>
       <div className="todo-title">
         <h1>Covid-19</h1>
       </div>
 
-
       <div className="todo-card-cima">
         <div className="cards-internos">
           <div className="primeira-linha-esquerda">
             <div className="text-card">
               <h3>O que é?</h3>
-              <p>A COVID-19 é uma doença causada pelo coronavírus SARS-CoV-2, que apresenta um quadro clínico que varia de infecções assintomáticas a quadros respiratórios graves. De acordo com a Organização Mundial de Saúde (OMS), a maioria dos pacientes com COVID-19 (cerca de 80%) podem ser assintomáticos e cerca de 20% dos casos podem requerer atendimento hospitalar por apresentarem dificuldade respiratória e desses casos aproximadamente 5% podem necessitar de suporte para o tratamento de insuficiência respiratória (suporte ventilatório).</p>
+              <p>
+                A COVID-19 é uma doença causada pelo coronavírus SARS-CoV-2, que
+                apresenta um quadro clínico que varia de infecções
+                assintomáticas a quadros respiratórios graves. De acordo com a
+                Organização Mundial de Saúde (OMS), a maioria dos pacientes com
+                COVID-19 (cerca de 80%) podem ser assintomáticos e cerca de 20%
+                dos casos podem requerer atendimento hospitalar por apresentarem
+                dificuldade respiratória e desses casos aproximadamente 5% podem
+                necessitar de suporte para o tratamento de insuficiência
+                respiratória (suporte ventilatório).
+              </p>
             </div>
           </div>
 
           <div className="primeira-linha-direita">
             <div className="text-card">
               <h3>Como é transmitido?</h3>
-              <h4>A transmissão acontece de uma pessoa doente para outra ou por contato próximo por meio de:</h4>
+              <h4>
+                A transmissão acontece de uma pessoa doente para outra ou por
+                contato próximo por meio de:
+              </h4>
               <ul>
                 <li>Toque do aperto de mão;</li>
                 <li>Gotículas de saliva;</li>
                 <li>Espirro;</li>
                 <li>Tosse;</li>
                 <li>Catarro;</li>
-                <li>Objetos ou superfícies contaminadas,
-                como celulares, mesas, maçanetas, brinquedos,
-
-                      teclados de computador etc.</li>
+                <li>
+                  Objetos ou superfícies contaminadas, como celulares, mesas,
+                  maçanetas, brinquedos, teclados de computador etc.
+                </li>
               </ul>
             </div>
           </div>
@@ -55,42 +65,51 @@ function Cuidados() {
               <h3>Como se previnir?</h3>
               <h4>
                 As recomendações de prevenção à COVID-19 são as seguintes:
-                </h4>
+              </h4>
               <ul>
                 <li>
-                  Lave com frequência as mãos até a altura dos punhos, com água e sabão, ou então higienize com álcool em gel 70%.
-                  </li>
+                  Lave com frequência as mãos até a altura dos punhos, com água
+                  e sabão, ou então higienize com álcool em gel 70%.
+                </li>
                 <li>
-                  Ao tossir ou espirrar, cubra nariz e boca com lenço ou com o braço, e não com as mãos.
-                  </li>
+                  Ao tossir ou espirrar, cubra nariz e boca com lenço ou com o
+                  braço, e não com as mãos.
+                </li>
                 <li>
                   Evite tocar olhos, nariz e boca com as mãos não lavadas.
-                  </li>
+                </li>
                 <li>
-                  Mantenha uma distância mínima de cerca de 2 metros de qualquer pessoa tossindo ou espirrando.
-                  </li>
+                  Mantenha uma distância mínima de cerca de 2 metros de qualquer
+                  pessoa tossindo ou espirrando.
+                </li>
                 <li>
-                  Higienize com frequência o celular e os brinquedos das crianças.
-                  </li>
+                  Higienize com frequência o celular e os brinquedos das
+                  crianças.
+                </li>
+                <li>Mantenha os ambientes limpos e bem ventilados.</li>
                 <li>
-                  Mantenha os ambientes limpos e bem ventilados.
-                  </li>
+                  Evite circulação desnecessária nas ruas, estádios, teatros,
+                  shoppings, shows, cinemas e igrejas. Se puder, fique em casa.
+                </li>
                 <li>
-                  Evite circulação desnecessária nas ruas, estádios, teatros, shoppings, shows, cinemas e igrejas. Se puder, fique em casa.
-                  </li>
+                  Se estiver doente, evite contato físico com outras pessoas,
+                  principalmente idosos e doentes crônicos, e fique em casa até
+                  melhorar.
+                </li>
                 <li>
-                  Se estiver doente, evite contato físico com outras pessoas, principalmente idosos e doentes crônicos, e fique em casa até melhorar.
-                  </li>
-                <li>
-                  Utilize máscaras caseiras ou artesanais feitas de tecido em situações de saída de sua residência.
-                  </li>
+                  Utilize máscaras caseiras ou artesanais feitas de tecido em
+                  situações de saída de sua residência.
+                </li>
               </ul>
             </div>
           </div>
           <div id="segunda-linha-direita" className="sub-card-direita">
             <div className="text-card">
               <h3>Quais são os sintomas</h3>
-              <h4>Os sintomas da COVID-19 podem variar de um simples resfriado até uma pneumonia severa. Sendo os sintomas mais comuns:</h4>
+              <h4>
+                Os sintomas da COVID-19 podem variar de um simples resfriado até
+                uma pneumonia severa. Sendo os sintomas mais comuns:
+              </h4>
               <ul className="esse">
                 <li>Tosse</li>
                 <li>Febre</li>
@@ -101,13 +120,10 @@ function Cuidados() {
             </div>
           </div>
         </div>
-
       </div>
       <div className="todo-footer">
         <h1>Footer</h1>
       </div>
-
-
     </div>
   );
 }
--- src/pages/contato/index.js
+++ src/pages/contato/index.js
@@ -1,68 +1,62 @@
-import React from 'react';
+// import React from 'react';
 
-import Logo from '../../components/logo-responsivo/index';
+// import Logo from '../../components/logo-responsivo/index';
 
-import Menu from "../../components/Menu/contatos";
+// import Menu from "../../components/Menu/contatos";
 
-import Rodape from '../../components/Rodape';
+// import Rodape from '../../components/Rodape';
 
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { fas } from '@fortawesome/free-solid-svg-icons';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+// import "./styles.css"
 
-import "./styles.css"
+// library.add(fas);
 
-library.add(fas);
+// function Contact() {
+//   return (
+//     <div id="pagecontato">
+//       <nav>
+//         <div>
+//           <Menu />
+//         </div>
+//       </nav>
+//       <div className="logo-responsiva-cont">
+//         <Logo />
+//       </div>
 
-function Contact() {
-  return (
-    <div id="pagecontato">
-      <nav>
-        <div>
-          <Menu />
-        </div>
-      </nav>
-      <div className="logo-responsiva-cont">
-        <Logo />
-      </div>
-      
-      <main id="maincontato">
-      
+//       <main id="maincontato">
 
-        <article>
+//         <article>
 
+//           <div className="cardcontato">
 
-          <div className="cardcontato">
+//             <h1>Contatos</h1>
+//             <ul>
+//               <li>
+//                 <a href="mailto:mapscovid19@gmail.com?" target="blank"><FontAwesomeIcon className="fass" icon="envelope" /> Email: mapscovid19@gmail.com</a>
+//               </li>
+//               <li>
+//                 <a href="https://www.instagram.com/jheycf/" target="blank"><FontAwesomeIcon className="fass" icon="camera-retro" /> Instagran: @jheycf</a>
+//               </li>
+//               <li>
+//                 <a href="http://www.facebook.com/112855440470064/" target="blank"><FontAwesomeIcon className="fass" icon="comments" /> Facebook: facebook.com/equipeJheyCF</a>
+//               </li>
+//             </ul>
 
-            <h1>Contatos</h1>
-            <ul>
-              <li>
-                <a href="mailto:mapscovid19@gmail.com?" target="blank"><FontAwesomeIcon className="fass" icon="envelope" /> Email: mapscovid19@gmail.com</a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/jheycf/" target="blank"><FontAwesomeIcon className="fass" icon="camera-retro" /> Instagran: @jheycf</a>
-              </li>
-              <li>
-                <a href="http://www.facebook.com/112855440470064/" target="blank"><FontAwesomeIcon className="fass" icon="comments" /> Facebook: facebook.com/equipeJheyCF</a>
-              </li>
-            </ul>
+//           </div>
 
-          </div>
+//         </article>
 
+//       </main>
+//       <div>
+//           <Rodape />
+//         </div>
 
+//     </div>
 
-        </article>
-        
-        
-      </main>
-      <div>
-          <Rodape />
-        </div>
+//   );
+// }
 
-    </div>
-
-  );
-}
-
-export default Contact;
+// export default Contact;
